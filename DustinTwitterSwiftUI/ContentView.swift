e6cd4eed69d269ce90787cbf14a9f29c7d05a0aa
DustinPark92 <p4569zz@gmail.com>
2020-12-20 20:07:29 +0900

    Initial Commit

---
+++ DustinTwitterSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DustinTwitterSwiftUI
+//
+//  Created by Dustin on 2020/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DustinTwitterSwiftUI/DustinTwitterSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DustinTwitterSwiftUIApp.swift
+//  DustinTwitterSwiftUI
+//
+//  Created by Dustin on 2020/12/20.
+//
+
+import SwiftUI
+
+@main
+struct DustinTwitterSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
